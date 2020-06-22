479938859084630d599f0047eabc6ce6eccd106a
Sylvain Roncoroni <sylvain.ronco@gmail.com>
2020-06-22 14:09:07 +0200

    Add comments

--- main.py
+++ main.py
@@ -9,20 +9,23 @@ def main():
 
     to_keep = []
     possible_levels = []
+    # Create the different possible levels on energy
+    # based on the total energy and the number of particles
     for j in range(1, energy - n + 1 + 1):
         possible_levels.append(j)
 
     for i in range(1, energy - n + 1 + 1):
+        # Get all the possible combinations
         for subset in itertools.combinations_with_replacement(possible_levels, n):
             _sum = 0
             for j in range(len(subset)):
                 _sum += subset[j]
 
+            # Only keep those that correspond to the total energy
             if _sum == energy and subset not in to_keep:
                 to_keep.append(subset)
 
     print(to_keep)
-    print(len(to_keep))
 
 
 if __name__ == '__main__':
