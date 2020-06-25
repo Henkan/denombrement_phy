24d1639bc259001238b1d8a380dfc95a6031274c
Sylvain Roncoroni <sylvain.ronco@gmail.com>
2020-06-25 17:13:48 +0200

    Rename interface and add image creation

--- denombrement.py
+++ denombrement.py
@@ -4,8 +4,33 @@ import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 from io import StringIO
 import sys
+from PIL import Image, ImageDraw
 
 from main import compute_macro_states
+
+
+def create_energy_diagram(n, energy, macro_states):
+    for i in range(len(macro_states)):
+        image = Image.new('RGB', (n * 100, energy * 100 + 10), (255, 255, 255))
+        draw = ImageDraw.Draw(image)
+        draw.line((0, image.size[1], image.size[0], image.size[1]), fill=128)
+        for x in range(1, energy + 1):
+            draw.line((0, image.size[1] - x * 100 + 5, image.size[0], image.size[1] - x * 100 + 5), fill=128,
+                      width=1)
+        e = macro_states[i]
+        for j in range(len(e.coeff)):
+            if e.coeff[j] != 0:
+                t = e.coeff[j]
+                j += 1
+                if t == 1:
+                    t = 2
+                else:
+                    t += 1
+                for x in range(1, t):
+                    draw.ellipse(
+                        ((image.size[0] / t) * x - 10, image.size[1] - j * 100 + 5 - 10, (image.size[0] / t) * x + 10,
+                         image.size[1] - j * 100 + 5 + 10), fill=(0, 0, 0))
+        image.save('diagram{0}.png'.format(i), 'PNG')
 
 
 def valide_nb(self, entry=None):
@@ -38,6 +63,8 @@ if __name__ == "__main__":
 
     label_niveau = Label(Mafenetre, text='Niveau de dégénérescence')
     label_niveau.grid(row='2', column='0')
+    explication_degenerescence = Label(Mafenetre, text="Par exemple: 2=3,3=4. (niveau 2 dégénéré 3 fois et niveau 3 dégénéré 4 fois)")
+    explication_degenerescence.grid(row='3', column='0')
 
     entry_degenerescence = Entry(Mafenetre, textvariable='nv_degenerescence')
     entry_degenerescence.grid(row='2', column='1')
@@ -46,19 +73,22 @@ if __name__ == "__main__":
     type_part = tk.IntVar()
 
     radioOne = tk.Radiobutton(Mafenetre, text='Classique', variable=type_part, value=1)
-    radioOne.grid(row='3', columnspan='2')
+    radioOne.grid(row='4', columnspan='2')
     radioOne.select()
     radioTwo = tk.Radiobutton(Mafenetre, text='Boson', variable=type_part, value=2)
-    radioTwo.grid(row='4', columnspan='2')
+    radioTwo.grid(row='5', columnspan='2')
     radioThree = tk.Radiobutton(Mafenetre, text='Fermion', variable=type_part, value=3)
-    radioThree.grid(row='5', columnspan='2')
+    radioThree.grid(row='6', columnspan='2')
 
-
+    image_choice = tk.IntVar()
+    create_image = tk.Checkbutton(Mafenetre, text="Créer diagrammes macro", variable=image_choice)
+    create_image.grid(row='7', columnspan='2')
 
     def run():
         n = int(entry_nb_particule.get())
         energy = int(entry_energy.get())
         particle_type = int(type_part.get())
+        create_diagram = int(image_choice.get())
         degeneration_input = entry_degenerescence.get()
         degeneration_input = degeneration_input.split(',')
 
@@ -71,7 +101,6 @@ if __name__ == "__main__":
             if tmp[0] != '':
                 degeneration[int(tmp[0]) - 1] = int(tmp[1])
 
-        # TODO: check examples
         macro_states = compute_macro_states(n, energy, degeneration)
 
         '''
@@ -111,31 +140,52 @@ if __name__ == "__main__":
 
             if len(most_probable) == 1:
                 print("l'état macroscopique le plus probable est le {0}."
-                      .format(most_probable[-1][0] + 1))
+                      .format(most_probable[-1][0]))
             else:
                 print("les états ", end='')
                 for i in range(len(most_probable) - 1):
-                    print("{0}, ".format(most_probable[i][0] + 1), end='')
+                    print("{0}, ".format(most_probable[i][0]), end='')
                 print('et {0} ont tous une probabilité de {1}.'
                       .format(most_probable[-1][0] + 1,
                               most_probable[-1][1] / sum(state.number_of_micro_states for state in macro_states)))
-
+            if create_diagram == 1:
+                create_energy_diagram(n, energy, macro_states)
         elif particle_type == 2:  # Bosons
             print("Avec n={0} bosons et E={1}, il y a {2} états macroscopiques équiprobables."
                   .format(n, energy, len(macro_states)))
+            if create_diagram:
+                create_energy_diagram(n, energy, macro_states)
         elif particle_type == 3:  # Fermions
             # Get number of states with only one particle
             nb_states = 0
-            for state in macro_states:
+            correct_states = []
+            correct_macro_states = []
+            for i in range(len(macro_states)):
+                state = macro_states[i]
                 keep = True
-                for e in state.coeff:
-                    if e not in [0, 1]:
+                for e in range(len(state.coeff)):
+                    if state.coeff[e] > degeneration[e]:
                         keep = False
+                    elif state.coeff[e] == degeneration[e]:
+                        pass  # TODO: handle micro
                 if keep:
+                    correct_states.append(i)
+                    correct_macro_states.append(macro_states[i])
                     nb_states += 1
 
-            print('Avec n={0} fermions et E={1}, il y a {2} états macroscopiques équiprobables.'
-                  .format(n, energy, nb_states))
+            print(
+                'Dans le cas des fermions, avec n={0} particules et E={1}, il y a {2} état(s) macroscopique(s) équiprobable(s) : '
+                .format(n, energy, nb_states), end='')
+
+            if len(correct_states) > 1:
+                for i in range(len(correct_states) - 1):
+                    print('le {0}, '.format(correct_states[i]), end='')
+                print('et le {0}.'.format(correct_states[-1]))
+            else:
+                print('le {0}.'.format(correct_states[-1]))
+
+            if create_diagram == 1:
+                create_energy_diagram(n, energy, correct_macro_states)
 
         sys.stdout = sys.__stdout__
         global text_out, Mafenetre
@@ -147,12 +197,12 @@ if __name__ == "__main__":
 
 
     run_button = Button(Mafenetre, text='Run', command=run)
-    run_button.grid(row='6', columnspan='2')
+    run_button.grid(row='8', columnspan='2')
 
     scrollbar = Scrollbar(Mafenetre)
 
     text_out = ScrolledText(Mafenetre, height=10, width=100, wrap="word")
     text_out.configure(state='disable')
-    text_out.grid(row='7', columnspan='2')
+    text_out.grid(row='9', columnspan='2')
 
     Mafenetre.mainloop()
